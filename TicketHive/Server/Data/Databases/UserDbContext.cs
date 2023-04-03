abf48ae1fadac318568fa3dc3f748105f5d4a2cb
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-04-03 12:53:48 +0200

    Initial commit

---
+++ TicketHive/Server/Data/Databases/UserDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TicketHive.Server.Models;
+
+namespace TicketHive.Server.Data.Databases
+{
+    public class UserDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public UserDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ TicketHive/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TicketHive.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
