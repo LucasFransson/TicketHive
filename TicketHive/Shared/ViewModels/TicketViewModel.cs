410be9d1e01805d1733af81c42eafa0fa1fbc803
Daniel Svensson <danielsvenssonn@gmail.com>
2023-04-04 14:37:38 +0200

    Adding ViewModels

---
+++ TicketHive/Shared/ViewModels/TicketViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketHive.Shared.ViewModels
+{
+    public class TicketViewModel
+    {
+        public int Id { get; set; }
+        public required EventViewModel Event { get; set; }
+        [MaxLength(100)]
+        public required string Username { get; set; }
+        public required decimal Price { get; set; }
+        public required DateTime StartTime { get; set; }
+        public required DateTime EndTime { get; set; }
+    }
+}
