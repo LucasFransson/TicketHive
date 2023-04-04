882782544a1564ffbd009066029ce7b1c64f1fa9
Daniel Svensson <danielsvenssonn@gmail.com>
2023-04-04 10:50:51 +0200

    replacing User with only Username in TicketModel

--- TicketHive/Server/Models/TicketModel.cs
+++ TicketHive/Server/Models/TicketModel.cs
@@ -1,10 +1,13 @@
-﻿namespace TicketHive.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketHive.Server.Models
 {
     public class TicketModel
     {
         public int Id { get; set; }
         public EventModel Event { get; set; }
-        public UserModel User { get; set; }
+        [MaxLength(100)]
+        public required string Username { get; set; }
         public decimal Price { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
--- TicketHive/Server/Models/UserModel.cs
+++ TicketHive/Server/Models/UserModel.cs
@@ -4,7 +4,6 @@ namespace TicketHive.Server.Models
 {
     public class UserModel : IdentityUser
     {
-        public List<TicketModel> Tickets { get; set; }
         public CountryModel Country { get; set; }
     }
 }
