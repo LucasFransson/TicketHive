fa57a1252998e196d77eb4acd9a2b94d8e1ed806
Daniel Svensson <danielsvenssonn@gmail.com>
2023-04-06 10:40:20 +0200

    fixing the country repo

--- TicketHive/Server/Data/Repositories/Interfaces/ICountryRepository.cs
+++ TicketHive/Server/Data/Repositories/Interfaces/ICountryRepository.cs
@@ -5,7 +5,7 @@ namespace TicketHive.Server.Data.Repositories.Interfaces
 {
     public interface ICountryRepository : IRepository<CountryModel>
     {
-        Task<IEnumerable<CountryModel>> GetAllAsync();
+        Task<List<CountryViewModel>> GetAllAsync();
         Task<CountryViewModel>? GetByName(string name);
     }
 }
