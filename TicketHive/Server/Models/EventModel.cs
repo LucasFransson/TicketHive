608b68d89edbb2035da6f64e5dcaddbf1ab77c3c
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-04-11 09:36:28 +0200

    Reset Merge

--- TicketHive/Server/Models/EventModel.cs
+++ TicketHive/Server/Models/EventModel.cs
@@ -15,7 +15,7 @@ namespace TicketHive.Server.Models
         public required decimal Price { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
-        public List<SoldTicketModel>? SoldTickets { get; set; }
+        public List<SoldTicketModel>? SoldTickets { get; set; } // T
 
         [ForeignKey(nameof(Country))]
         public required string CountryName { get; set; }
