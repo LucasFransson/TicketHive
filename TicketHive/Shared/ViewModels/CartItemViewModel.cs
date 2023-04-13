5601e89f524628584af14723276c688c6aa1401a
Daniel Svensson <danielsvenssonn@gmail.com>
2023-04-13 11:31:14 +0200

    pushing only to save changes

---
+++ TicketHive/Shared/ViewModels/CartItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketHive.Shared.ViewModels;
+public class CartItemViewModel
+{
+    public int EventId { get; set; }
+    public string? EventName { get; set; }
+    public decimal Price { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public int Quantity { get; set; }
+}
